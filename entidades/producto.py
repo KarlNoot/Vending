8bd70db106b0340c41a8be9cc699c8012852e3f0
KarlNoot <Luiscrm16@gmail.com>
2026-03-24 11:09:35 -0700

    Version 2

---
+++ entidades/producto.py
@@ -0,0 +1,14 @@
+from enums.tipo_producto import TipoProducto
+
+class Producto:
+    def __init__ (self, nombre: str, precio_unitario: float, cantidad_existencias: int, tipo_producto: TipoProducto):
+        self.nombre = nombre
+        self.precio_unitario = precio_unitario
+        self.cantidad_existencias = cantidad_existencias
+        self.tipo_producto = tipo_producto
+
+    def comprar_producto(self):
+        if self.cantidad_existencias > 0:
+            self.cantidad_existencias -= 1
+            operacion = self.precio_unitario - #Variable carlos
+            return f"Compra procesada del articulo. Precio: {self.precio_unitario})
